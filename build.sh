bf015efa5e9de202789ad794e1a33c0ec8848a6a
Mark Nash <abacefabmicroblast@hotmail.com>
2025-09-05 14:38:07 -0700

    Add sudo

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -u
 set -o pipefail
 set -x
 
-apt install golang-go
+sudo apt install golang-go
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 xcaddy build --with github.com/caddy-dns/azure
