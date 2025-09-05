0a2508419405961e8532b1a6a7434439bd87cef1
Mark Nash <abacefabmicroblast@hotmail.com>
2025-09-05 14:51:11 -0700

    Install the latest version of go

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@ set -u
 set -o pipefail
 set -x
 
-sudo apt install golang-go
-go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
+VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n 1)
+wget https://go.dev/dl/${VERSION}.linux-amd64.tar.gz -O go.tar.gz
+tar xf go.tar.gz
+rm go.tar.gz
 export PATH=$PATH:$HOME/go/bin
+
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 xcaddy build --with github.com/caddy-dns/azure
+
+
